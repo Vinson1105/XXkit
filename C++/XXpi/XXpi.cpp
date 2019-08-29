c80d6a21774b807d1dd793f14e01d7bc5d2a7de1
vinson1105 <vinson1105@foxmail.com>
2019-08-29 17:10:05 +0800

    微调

--- C++/XXpi/XXpi.cpp
+++ C++/XXpi/XXpi.cpp
@@ -1,7 +1,8 @@
 #include "XXpi.h"
 
-#define HEADER_KEY "XXenterPi" 
-#define END_STRING "\r\n"
+#define FIRST_KEY      "XXenterPi" 
+#define SPLIT_KV        ":"
+#define SPLIT_PAIRS     "\r\n"
 
 XXpi::XXpi(const string &subPi)
     : _subPi(subPi){
@@ -9,8 +10,13 @@ XXpi::XXpi(const string &subPi)
 XXpi::~XXpi(){}
 
 const string& XXpi::getData(){
-    string data;
-    auto iter = map
+    string data = FIRST_KEY;
+    data += SPLIT_KV; data += _subPi; data += SPLIT_PAIRS;
+    for (map<string,string>::iterator iter = _map.begin(); iter != _map.end(); iter++)
+    {
+        data += 
+    }
+    
 }
 bool XXpi::parseData(const int8_t *data, int length, map<string,string> &map){
     
