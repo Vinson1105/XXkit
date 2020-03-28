119cf91484b5b131da88cf613093d7598516bab3
vinson1105 <vinson1105@foxmail.com>
2020-03-28 16:07:09 +0800

    删除冗余的Base和Protocol，简化层次，增加注释

--- Object-C/AV/XXh26xDecoder.h
+++ Object-C/AV/XXh26xDecoder.h
@@ -1,19 +1,37 @@
+/**
+ * 20200-03-28
+ * 1、删除冗余的Base和Protocol，简化层次
+ * 2、加上必要性注释
+ */
+
 #import <Foundation/Foundation.h>
 #import <VideoToolbox/VideoToolbox.h>
 
 NS_ASSUME_NONNULL_BEGIN
-@protocol XXdecoderDerived <NSObject>
-@optional - (CVPixelBufferRef) toDecodeData:(uint8_t*)data Length:(int)length;
-@optional - (BOOL) toDecodeSrcData:(uint8_t*)srcData SrcLength:(int)srcLength DestBuffer:(uint8_t*)destBuffer DestLength:(int*)destLength;
-@end
+@interface XXh26xDecoder : NSObject
+/**
+ * @brief    创建h264或者h265硬件解码器
+ * @param    h264 YES：创建h264解码器，NO：创建h265解码器
+ * @return   返回nil，创建失败；否则返回解码器实例
+ */
++ (XXh26xDecoder*)decoderWithH264:(BOOL)h264;
 
-@interface XXdecoderBase : NSObject
-@property (nonatomic,copy,readonly) NSString *name;
-- (CVPixelBufferRef) decodeData:(uint8_t*)data Length:(int)length;
-- (BOOL) decodeSrcData:(uint8_t*)srcData SrcLength:(int)srcLength DestBuffer:(uint8_t*)destBuffer DestLength:(int*)destLength;
-@end
+/**
+ * @brief    解码数据，同步返回，并返回【CVPixelBufferRef】,需要自主对【CVPixelBufferRetain】和【CVPixelBufferRelease】
+ * @param    data 待解码的原始数据，数据中的nalu之间需要有【0x00 0x00 0x00 0x01】
+ * @param    length 原始数据长度
+ * @return   失败返回nil
+ */
+- (CVPixelBufferRef)decodeData:(uint8_t*)data length:(int)length;
 
-@interface XXh26xDecoder : XXdecoderBase
-+(XXh26xDecoder*) decoderWithName:(NSString*)name IsH264:(BOOL)isH264;
+/**
+ * @brief    解码数据，同步返回，需要预先分配destBuffer
+ * @param    srcData 待解码的原始数据，数据中的nalu之间需要有【0x00 0x00 0x00 0x01】
+ * @param    srcLength 原始数据长度
+ * @param    destBuffer 存放解码后数据的buffer
+ * @param    destLength 解码后数据长度
+ * @return   失败返回NO，否则返回YES
+ */
+- (BOOL)decodeSrcData:(uint8_t*)srcData srcLength:(int)srcLength destBuffer:(uint8_t*)destBuffer destLength:(int*)destLength;
 @end
 NS_ASSUME_NONNULL_END
--- Object-C/AV/XXh26xDecoder.m
+++ Object-C/AV/XXh26xDecoder.m
@@ -1,17 +1,15 @@
-//
-//  XXh26xDecoder.m
-//  NVSDK
-//
-//  Created by VINSON on 2019/7/29.
-//  Copyright © 2019 macrovideo. All rights reserved.
-//
-
 #import "XXh26xDecoder.h"
-#include "../../C/XXnaluSpliter.h"
+#include "XXnaluSpliter.h"
 
 //#define USING_DECODETIME_MARKET
 #define DEFAULT_FRAME_BUFFER_SIZE   512*1024
+
+#define OUTPUT_YUV420   /// 是否输出yuv420格式
+#ifdef OUTPUT_YUV420
+#define DEFAULT_PIXEL_FORMAT        kCVPixelFormatType_420YpCbCr8Planar
+#else
 #define DEFAULT_PIXEL_FORMAT        kCVPixelFormatType_24RGB
+#endif
 
 #pragma mark - C函数声明
 static void decodeOutputCallBack(void *decompressionOutputRefCon,
@@ -23,68 +21,32 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
                                  CMTime presentationDuration);
 
 #
-#pragma mark - 类: <XXdecoderBase>
-#
-@interface XXdecoderBase()
-@property (nonatomic,weak) id<XXdecoderDerived> derivedClass;
-@end
-@implementation XXdecoderBase
-- (instancetype)init{
-    self = [super init];
-    if (self) {
-        _derivedClass = self;
-    }
-    return self;
-}
-- (instancetype) initWithName:(NSString*)name{
-    self = [self init];
-    if (self) {
-        _name   = name;
-    }
-    return self;
-}
-- (CVPixelBufferRef) decodeData:(uint8_t*)data Length:(int)length{
-    if ([_derivedClass respondsToSelector:@selector(toDecodeData:Length:)]) {
-        return [_derivedClass toDecodeData:data Length:length];
-    }
-    return nil;
-}
-- (BOOL) decodeSrcData:(uint8_t*)srcData SrcLength:(int)srcLength DestBuffer:(uint8_t*)destBuffer DestLength:(int*)destLength{
-    if ([_derivedClass respondsToSelector:@selector(toDecodeSrcData:SrcLength:DestBuffer:DestLength:)]) {
-        return [_derivedClass toDecodeSrcData:srcData SrcLength:srcLength DestBuffer:destBuffer DestLength:destLength];
-    }
-    return NO;
-}
-@end
-
-#
 #pragma mark - 类: <XXh26xDecoder>
 #
-@interface XXh26xDecoder()<XXdecoderDerived>
-@property (nonatomic,assign) BOOL isInited;
-@property (nonatomic,assign) BOOL isH264;
-@property (nonatomic,assign) XXnaluSpliterHandle spliterHandle;
+@interface XXh26xDecoder()
+@property (nonatomic,assign) BOOL isInited;     /// 是否已初始化
+@property (nonatomic,assign) BOOL isH264;       /// 是否为h264
+@property (nonatomic,assign) XXnaluSpliterHandle spliterHandle; /// nalu拆分工具
 
-@property (nonatomic,assign) CMVideoFormatDescriptionRef descriptionRef;
-@property (nonatomic,assign) VTDecompressionSessionRef sessionRef;
-@property (nonatomic,assign) CVPixelBufferRef pixelBufferRef;
+@property (nonatomic,assign) CMVideoFormatDescriptionRef descriptionRef;    /// 解码器格式描述对象
+@property (nonatomic,assign) VTDecompressionSessionRef sessionRef;          /// 解码会话对象
+@property (nonatomic,assign) CVPixelBufferRef pixelBufferRef;               /// 解码输出buffer
 
-@property (nonatomic,strong) NSData *vps;
-@property (nonatomic,strong) NSData *sps;
-@property (nonatomic,strong) NSData *pps;
+@property (nonatomic,strong) NSData *vps;           /// 数据中vps（h265特有）
+@property (nonatomic,strong) NSData *sps;           /// 数据中sps
+@property (nonatomic,strong) NSData *pps;           /// 数据中pps
 @property (nonatomic,assign) uint8_t *frameBuffer;
 @end
 @implementation XXh26xDecoder
 #pragma mark - 构建析构
-+(XXh26xDecoder*) decoderWithName:(NSString*)name IsH264:(BOOL)isH264{
-    XXh26xDecoder *decoder  = [[XXh26xDecoder alloc] initWithName:name IsH264:isH264];
-    return decoder;
++ (XXh26xDecoder*)decoderWithH264:(BOOL)h264{
+    return [[XXh26xDecoder alloc] initWithH264:h264];
 }
-- (instancetype) initWithName:(NSString*)name IsH264:(BOOL)isH264{
-    self = [super initWithName:name];
+- (instancetype) initWithH264:(BOOL)h264{
+    self = [super init];
     if (self) {
         _isInited       = NO;
-        _isH264         = isH264;
+        _isH264         = h264;
         _spliterHandle  = xxnaluSpliter_create(10);
         
         _sessionRef     = nil;
@@ -100,11 +62,97 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
 }
 - (void)dealloc{
     xxnaluSpliter_free(&_spliterHandle);
+    _spliterHandle = NULL;
+    
     if(nil != _pixelBufferRef)  { CFRelease(_pixelBufferRef);   _pixelBufferRef = nil; }
     if(nil != _descriptionRef)  { CFRelease(_descriptionRef);   _descriptionRef = nil; }
     if(nil != _sessionRef)      { CFRelease(_sessionRef);       _sessionRef = nil; }
+    
     free(_frameBuffer);
     _frameBuffer = NULL;
+}
+
+#pragma mark - 公共接口: <解码>
+- (CVPixelBufferRef)decodeData:(uint8_t*)data length:(int)length{
+#ifdef USING_DECODETIME_MARKET
+    CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
+#endif
+    
+    /// 数据有效性判断
+    if (NULL == data || length <= 0) {
+        NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
+        return nil;
+    }
+    
+    /// nalu分割
+    int naluCount = xxnaluSpliter_split(_spliterHandle, data, length, _isH264);
+    if (naluCount <= 0) {
+        NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
+        return nil;
+    }
+    
+    /// 解码器初始化
+    if (!_isInited) {
+        if([self initDecode]){
+            _isInited = YES;
+        }
+        else{
+            NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
+            return nil;
+        }
+    }
+    
+    /// 取出I帧或者P帧
+    uint32_t frameLength = 0;
+    [self mediaDataUsingSpliterHandle:_spliterHandle Buffer:_frameBuffer Length:&frameLength];
+    if (0 == frameLength) {
+        NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
+        return nil;
+    }
+    
+    /// 将原始数据转换到【CMSampleBufferRef】
+    CMSampleBufferRef sampleBufferRef = nil;
+    if(![self toSampleBufferRef:&sampleBufferRef FrameData:_frameBuffer Length:frameLength DescriptionRef:_descriptionRef]){
+        NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
+        return nil;
+    }
+    
+    /// 执行解码，现使用的同步解码，在回调中对【self.pixelBufferRef】进行赋值，故在本函数可以直接返回【self.pixelBufferRef】
+    VTDecodeFrameFlags flags = 0;
+    VTDecodeInfoFlags flagOut = 0;
+    OSStatus status = VTDecompressionSessionDecodeFrame(_sessionRef,
+                                                        sampleBufferRef,
+                                                        flags,
+                                                        (__bridge void*)(self),
+                                                        &flagOut);
+    
+    CFRelease(sampleBufferRef);
+    if (noErr != status) {
+        switch (status) {
+            case kVTInvalidSessionErr:      [self resetDecoder]; break;
+            case kVTVideoDecoderBadDataErr: break;
+            default:break;
+        }
+        NSLog(@"[XXh26xDecorder] 错误 status:%d (LINE:%d).", (int)status, __LINE__);
+        return nil;
+    }
+#ifdef USING_DECODETIME_MARKET
+    CFAbsoluteTime timeAfterDecode = CFAbsoluteTimeGetCurrent();
+    NSLog(@"[XXh26xDecoder] Length:%d Decode:%.3f.", length, timeAfterDecode-startTime);
+#endif
+    return _pixelBufferRef;
+}
+- (BOOL)decodeSrcData:(uint8_t*)srcData srcLength:(int)srcLength destBuffer:(uint8_t*)destBuffer destLength:(int*)destLength{
+    CVPixelBufferRef bufferRef = [self decodeData:srcData length:srcLength];
+    if (nil == bufferRef) {
+        return NO;
+    }
+#ifdef OUTPUT_YUV420
+    *destLength = [self yuv420PixelBuffer:bufferRef toBuffer:destBuffer];
+#else
+    *destLength = [self rgb24PixelBuffer:bufferRef toBuffer:destBuffer];
+#endif
+    return YES;
 }
 
 #pragma mark - 属性读写: <PixelBufferRef>
@@ -120,36 +168,22 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
     }
 }
 
-#pragma mark - 协议函数: <XXdecoderDerived>
-- (CVPixelBufferRef)toDecodeData:(uint8_t *)data Length:(int)length{
-    return [self decodeData:data Length:length];
+#pragma mark - 私有函数: <初始化解码器> <创建解码器描述文件> <创建解码器会话> <采样样本转换>
+- (void)resetDecoder{
+    if(!_isInited)  return;
+    _vps = nil; _sps = nil; _pps = nil; _isInited = NO;
 }
-- (BOOL) toDecodeSrcData:(uint8_t*)srcData SrcLength:(int)srcLength DestBuffer:(uint8_t*)destBuffer DestLength:(int*)destLength{
-    CVPixelBufferRef bufferRef = [self decodeData:srcData Length:srcLength];
-    if (nil == bufferRef) {
-        return NO;
-    }
-
-    CVPixelBufferLockBaseAddress(bufferRef, kCVPixelBufferLock_ReadOnly);
-    void *dataAddress   = CVPixelBufferGetBaseAddressOfPlane(bufferRef, 0);
-    *destLength         = (int)CVPixelBufferGetDataSize(bufferRef);
-    memcpy(destBuffer, dataAddress, *destLength);
-    CVPixelBufferUnlockBaseAddress(bufferRef,0);
-    return YES;
-}
-
-#pragma mark - 私有函数: <初始化解码器> <创建解码器描述文件> <创建解码器会话> <采样样本转换> <提取媒体信息(VPS/SPS/PPS)> <提取画面数据(I/P)>
 - (BOOL)initDecode{
-    [self mediaInfoSpliterHandle:_spliterHandle VPS:&_vps SPS:&_sps PPS:&_pps];
+    [self mediaInfoSpliterHandle:_spliterHandle vps:&_vps sps:&_sps pps:&_pps];
     if ((!_isH264 && nil == _vps) || nil == _sps || nil == _pps) {
-        //GWXLog(@"[XXh26xDecorder] 错误(%d) %d %d %d", __LINE__, (!_isH264 && nil == _vps), nil == _sps, nil == _pps);
+        NSLog(@"[XXh26xDecorder] 错误 %d %d %d (LINE:%d).", (!_isH264 && nil == _vps), nil == _sps, nil == _pps, __LINE__);
         return NO;
     }
     
     if(nil != _descriptionRef){ CFRelease(_descriptionRef); _descriptionRef = nil; }
-    _descriptionRef = [self createDescriptionUsingVPS:_vps SPS:_sps PPS:_pps IsH264:_isH264];
+    _descriptionRef = [self createDescriptionWithVps:_vps sps:_sps pps:_pps h264:_isH264];
     if (nil == _descriptionRef) {
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
+        NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
         return NO;
     }
     
@@ -158,15 +192,15 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
     if (nil == _sessionRef) {
         CFRelease(_descriptionRef);
         _descriptionRef = nil;
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
+        NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
         return NO;
     }
     return YES;
 }
-- (CMVideoFormatDescriptionRef) createDescriptionUsingVPS:(NSData*)vps
-                                                      SPS:(NSData*)sps
-                                                      PPS:(NSData*)pps
-                                                   IsH264:(BOOL)isH264{
+- (CMVideoFormatDescriptionRef) createDescriptionWithVps:(NSData*)vps
+                                                     sps:(NSData*)sps
+                                                     pps:(NSData*)pps
+                                                    h264:(BOOL)isH264{
     CMVideoFormatDescriptionRef descriptionRef  = nil;
     OSStatus status = -1;
     if (isH264) {
@@ -191,6 +225,7 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
                                                                      &descriptionRef);
     }
     else{
+        NSLog(@"[XXh26xDecorder] 错误,系统版本过低 (LINE:%d).", __LINE__);
     }
     return noErr == status ? descriptionRef : nil;
 }
@@ -253,7 +288,9 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
     CFRelease(sampleBufferRef);
     return YES;
 }
-- (void) mediaInfoSpliterHandle:(XXnaluSpliterHandle)spliterHandle VPS:(NSData* __strong*)vps SPS:(NSData* __strong*)sps PPS:(NSData* __strong*)pps{
+
+#pragma mark - 私有函数: <在nalu中获取vps、sps、pps> <在nalu中获取I帧或者P帧> <yuv420->rawData> <rgb24->rawData>
+- (void) mediaInfoSpliterHandle:(XXnaluSpliterHandle)spliterHandle vps:(NSData* __strong*)vps sps:(NSData* __strong*)sps pps:(NSData* __strong*)pps{
     uint8_t *dataRef    = NULL;
     uint32_t dataLength = 0;
     xxnaluSpliter_vpsRef(spliterHandle, &dataRef, &dataLength, false);
@@ -278,7 +315,7 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
         xxnaluSpliter_pframeRef(_spliterHandle, &dataRef, &dataRefLength, false);
     }
     if (NULL == dataRef || 0 == dataRefLength) {
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
+        NSLog(@"[XXh26xDecorder] 错误 (LINE:%d).", __LINE__);
         *length = 0;
         return;
     }
@@ -288,68 +325,41 @@ static void decodeOutputCallBack(void *decompressionOutputRefCon,
     memcpy(buffer+4, dataRef, dataRefLength);
     *length = dataRefLength + 4;
 }
-
-#pragma mark - 私有函数: <解码>
-- (CVPixelBufferRef) decodeData:(uint8_t*)data Length:(int)length{
-#ifdef USING_DECODETIME_MARKET
-    CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
-#endif
-    if (NULL == data || length <= 0) {
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
-        return nil;
-    }
+- (int)yuv420PixelBuffer:(CVPixelBufferRef)pixelBuffer toBuffer:(uint8_t*)buffer{
+    CVPixelBufferLockBaseAddress(pixelBuffer, kCVPixelBufferLock_ReadOnly);
     
-    int naluCount = xxnaluSpliter_split(_spliterHandle, data, length, _isH264);
-    if (naluCount <= 0) {
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
-        return nil;
-    }
+    void *yAddress = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
+    void *uAddress = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 1);
+    void *vAddress = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 2);
     
-    if (!_isInited) {
-        if([self initDecode]){
-            _isInited = YES;
-        }
-        else{
-            //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
-            return nil;
-        }
-    }
+    size_t yBytePerRow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 0);
+    size_t uBytePerRow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 1);
+    size_t vBytePerRow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 2);
     
-    uint32_t frameLength = 0;
-    [self mediaDataUsingSpliterHandle:_spliterHandle Buffer:_frameBuffer Length:&frameLength];
-    if (0 == frameLength) {
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
-        return nil;
-    }
-    CMSampleBufferRef sampleBufferRef = nil;
-    if(![self toSampleBufferRef:&sampleBufferRef FrameData:_frameBuffer Length:frameLength DescriptionRef:_descriptionRef]){
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
-        return nil;
-    }
+    size_t w = CVPixelBufferGetWidth(pixelBuffer);
+    size_t h = CVPixelBufferGetHeight(pixelBuffer);
+    uint8_t *y, *u, *v;
     
-    VTDecodeFrameFlags flags = 0;
-    VTDecodeInfoFlags flagOut = 0;
-    OSStatus status = VTDecompressionSessionDecodeFrame(_sessionRef,
-                                                        sampleBufferRef,
-                                                        flags,
-                                                        (__bridge void*)(self),
-                                                        &flagOut);
+    y = buffer;
+    u = y + w * h;
+    v = u + w * h / 4;
+    for (int i=0; i<h; i++)
+        memcpy(y + w * i, yAddress + yBytePerRow * i, w);
+    for (int i=0; i<h/2; i++)
+        memcpy(u + w / 2 * i, uAddress + uBytePerRow * i, w / 2);
+    for (int i=0; i<h/2; i++)
+        memcpy(v + w / 2 * i, vAddress + vBytePerRow * i, w / 2);
     
-    CFRelease(sampleBufferRef);
-    if (noErr != status) {
-        switch (status) {
-            case kVTInvalidSessionErr:      break;
-            case kVTVideoDecoderBadDataErr: break;
-            default:break;
-        }
-        //GWXLog(@"[XXh26xDecorder] 错误(%d)", __LINE__);
-        return nil;
-    }
-#ifdef USING_DECODETIME_MARKET
-    CFAbsoluteTime timeAfterDecode  = CFAbsoluteTimeGetCurrent();
-    GWXLog(@"[XXh26xDecoder] Length:%d Decode:%.3f", length, timeAfterDecode-startTime);
-#endif
-    return _pixelBufferRef;
+    CVPixelBufferUnlockBaseAddress(pixelBuffer,0);
+    return (int)(w * h *3 / 2);
+}
+- (int)rgb24PixelBuffer:(CVPixelBufferRef)pixelBuffer toBuffer:(int8_t*)buffer{
+    CVPixelBufferLockBaseAddress(pixelBuffer, kCVPixelBufferLock_ReadOnly);
+    void *dataAddress    = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
+    size_t length        = (int)CVPixelBufferGetDataSize(pixelBuffer);
+    memcpy(pixelBuffer, dataAddress, length);
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    return (int)length;
 }
 @end
 
