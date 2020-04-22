0c40f5c0370d943a8f28e4110e4ee5229dae6a08
Vinson1105 <386820683@qq.com>
2020-04-23 07:04:56 +0800

    完成度（50%）

--- Object-C/UITextField/XXtextFieldShell.h
+++ Object-C/UITextField/XXtextFieldShell.h
@@ -1,25 +1,33 @@
-//
-//  XXtextFieldShell.h
-//  XXkitProject
-//
-//  Created by VINSON on 2020/4/22.
-//  Copyright © 2020 郭文轩. All rights reserved.
-//
+/**
+ * 2020-04-22  对UITextField的简单封装
+ * 1、正则表达式实时判断
+ * 2、使用image简单配置left view或者right view
+ */
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef NS_ENUM(NSUInteger, XXtextFieldShellState) {
-    XXtextFieldShellStateEditBegin,
-    XXtextFieldShellStateEditEnd,
-};
-
-@interface XXtextFieldShell : NSObject
+@interface XXtextFieldShell : NSObject<UITextFieldDelegate>
+/** 目标UITextField */
 @property (nonatomic,readonly,weak) UITextField *target;
-@property (nonatomic,copy,nullable) NSString *express;
-@property (nonatomic,copy,nullable) void(^onStateChanged)(XXtextFieldShell *shell,)
+/** 正则表达式 */
+@property (nonatomic,copy,nullable) NSString *expression;
+/** UITextField的text改变 */
+@property (nonatomic,copy,nullable) void(^onTextChanged)(XXtextFieldShell *shell, NSString *text);
+/** UITextField的开始编辑 */
+@property (nonatomic,copy,nullable) void(^onEditBegin)(XXtextFieldShell *shell);
+/** UITextField的结束编辑 */
+@property (nonatomic,copy,nullable) void(^onEditEnd)(XXtextFieldShell *shell);
+
+/**
+ * @brief 设置UITextField
+ */
 - (void)shell:(UITextField*)target;
+
+/**
+ * @brief 使用image配置UITextField的leftView或者rightView
+ */
 - (void)configImage:(UIImage*)image mode:(UITextFieldViewMode)mode left:(BOOL)left;
 @end
 
--- Object-C/UITextField/XXtextFieldShell.m
+++ Object-C/UITextField/XXtextFieldShell.m
@@ -1,13 +1,78 @@
-//
-//  XXtextFieldShell.m
-//  XXkitProject
-//
-//  Created by VINSON on 2020/4/22.
-//  Copyright © 2020 郭文轩. All rights reserved.
-//
-
 #import "XXtextFieldShell.h"
 
-@implementation XXtextFieldShell
+@interface XXtextFieldShell()
+@property (nonatomic,copy,nullable) NSString *lastText;     // 上一次string
+@property (nonatomic,strong) NSPredicate *predicate;        // 需要匹配的正则表达式
+@end
 
+@implementation XXtextFieldShell
+#pragma mark - <Init>
+-(void)shell:(UITextField*)target{
+    __strong typeof(_target) strongTarget = _target;
+    if(strongTarget){
+        strongTarget.delegate = nil;
+        [strongTarget removeTarget:self action:@selector(onTextFieldEditingChanged:) forControlEvents:UIControlEventEditingChanged];
+    }
+    _target = target;
+    strongTarget = _target;
+    if(strongTarget){
+        strongTarget.delegate = self;
+        [strongTarget addTarget:self action:@selector(onTextFieldEditingChanged:) forControlEvents:UIControlEventEditingChanged];
+    }
+}
+
+#pragma mark - <Public>
+- (void)configImage:(UIImage*)image mode:(UITextFieldViewMode)mode left:(BOOL)left{
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    if(left){
+        _target.leftView = imageView;
+        _target.leftViewMode = mode;
+    }
+    else{
+        _target.rightView = imageView;
+        _target.rightViewMode = mode;
+    }
+}
+
+#pragma mark - <Property>
+- (void)setExpression:(NSString *)expression{
+    _expression = expression;
+    _predicate = nil == expression ? nil : [NSPredicate predicateWithFormat:@"SELF MATCHES %@", expression];
+}
+
+#pragma mark - <Event>
+-(void)onTextFieldEditingChanged:(id)sender{
+    if(nil == _predicate) return;
+    
+    UITextField *textField = (UITextField*)sender;
+    NSString *currentText = textField.text;
+    if([_predicate evaluateWithObject:currentText]){
+        _lastText = textField.text;
+        if (_onTextChanged) {
+            _onTextChanged(self,currentText);
+        }
+    }
+    else{
+        textField.text = _lastText;
+    }
+}
+
+#pragma mark - <UITextFieldDelegate>
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+- (BOOL)textFieldShouldClear:(UITextField *)textField{
+    return YES;
+}
+- (void)textFieldDidBeginEditing:(UITextField *)textField{
+    if(_onEditBegin){
+        _onEditBegin(self);
+    }
+}
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    if(_onEditEnd){
+        _onEditEnd(self);
+    }
+}
 @end
