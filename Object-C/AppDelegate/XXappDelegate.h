b68e537ae876bd2ebf64b9230ed423c42e2eeeb1
vinson1105 <vinson1105@foxmail.com>
2020-05-05 15:10:05 +0800

    新增Scene相关处理

--- Object-C/AppDelegate/XXappDelegate.h
+++ Object-C/AppDelegate/XXappDelegate.h
@@ -1,15 +1,19 @@
 /**
- * 2020-04-14
- * 1、新增创建根视图接口
- *
- * 2020-04-11
- * 1、新增创建导航栏接口
- * 2、新增window属性，在创建导航栏时会对此属性初始化，并显示该window
- * 3、实现applicationLaunchFinished函数，但只是单纯返回YES，继承类重载是需要调用super的实现，方便日后扩展
- *
- * 2020-03-25
- *  1、旋转控制，兼容非主线程调用
- *  2、app状态通知，配合【UIViewController+ApplicationState】使用
+ 2020-05-05
+ 1、移除关于创建Navigation冗余接口
+ 2、增加Scene的相关接口兼容（详细请看目录下的Readme）
+ 
+ 2020-04-14
+ 1、新增创建根视图接口
+ 
+ 2020-04-11
+ 1、新增创建导航栏接口
+ 2、新增window属性，在创建导航栏时会对此属性初始化，并显示该window
+ 3、实现applicationLaunchFinished函数，但只是单纯返回YES，继承类重载是需要调用super的实现，方便日后扩展
+ 
+ 2020-03-25
+ 1、旋转控制，兼容非主线程调用
+ 2、app状态通知，配合【UIViewController+ApplicationState】使用
  */
 
 #import <UIKit/UIKit.h>
@@ -19,43 +23,29 @@ NS_ASSUME_NONNULL_BEGIN
 @interface XXappDelegate : UIResponder<UIApplicationDelegate>
 @property (nonatomic,strong) UIWindow *window;
 /**
- * @brief 锁定旋转
- * @param lock YES：锁定，NO：解锁
+ 锁定旋转
+ @param lock YES：锁定，NO：解锁
  */
 + (void)rotationLock:(BOOL)lock;
 
 /**
- * @brief 获取旋转锁定状态
- * @return 返回YES表示已锁定
+ 获取旋转锁定状态
+ @return 返回YES表示已锁定
  */
 + (BOOL)isRotationLocked;
 
 /**
- * @brief 创建一个导航栏
- * @param root 创建导航栏时使用的ViewController
+ 创建一个导航栏
+ @param root 创建导航栏时使用的ViewController
  */
 - (void)createNavigationWithRootViewController:(UIViewController*)root;
 
 /**
- * @brief 创建一个导航栏，并以指定的类名创建根视图
- * @param root 导航栏的根视图类名
- * @param type 视图的加载方式，有UIStoryboard和Xib两种
- * @param bundle 文件对应的包
+ 创建一个导航栏，并指定使用UIStoryboard的初始化初始化入口作为根视图
+ @param storyboard 使用的UIStoryboard的文件名
+ @param bundle 文件对应的包
  */
-- (void)createNavigationWithRootViewController:(NSString*)root type:(NSString*)type bundle:(nullable NSBundle*)bundle;
-
-/**
- * @brief 创建一个导航栏，并指定使用UIStoryboard的初始化初始化入口作为根视图
- * @param storyboard 使用的UIStoryboard的文件名
- * @param bundle 文件对应的包
- */
-- (void)createNavigationWithUIStoryboard:(NSString*)storyboard bundle:(nullable NSBundle*)bundle;
-
-/**
- * @brief 创建一个导航栏，并指定使用nib文件作为根视图
- * @param bundle 文件对应的包
- */
-- (void)createNavigationWithNibClass:(NSString*)nib bundle:(nullable NSBundle*)bundle;
+- (void)createNavigationWithIntitalViewControllerAtStoryboard:(NSString*)storyboard bundle:(nullable NSBundle*)bundle;
 @end
 
 NS_ASSUME_NONNULL_END
--- Object-C/AppDelegate/XXappDelegate.m
+++ Object-C/AppDelegate/XXappDelegate.m
@@ -1,11 +1,3 @@
-//
-//  XXappDelegate.m
-//  iCamSee
-//
-//  Created by VINSON on 2020/3/26.
-//  Copyright © 2020 Macrovideo. All rights reserved.
-//
-
 #import "XXappDelegate.h"
 
 #define kSigApplicationWillResignActive     @"sigApplicationWillResignActive"
@@ -13,14 +5,14 @@
 #define kSigApplicationDidBecomeActive      @"sigApplicationDidBecomeActive"
 #define kSigApplicationWillEnterForeground  @"sigApplicationWillEnterForeground"
 
-#define kUIStoryboard   @"UIStoryboard"
-#define kXib            @"Xib"
+#define isUsingScene 0
 
 @interface XXappDelegate()
 @property (nonatomic,assign) BOOL rotationLock;
 @end
 
 @implementation XXappDelegate
+#pragma mark - 横竖屏锁定
 + (void)rotationLock:(BOOL)lock{
     if ([NSThread currentThread].isMainThread) {
         XXappDelegate *appDelegate = (XXappDelegate*)[UIApplication sharedApplication].delegate;
@@ -47,18 +39,16 @@
         return isLocked;
     }
 }
+
+#pragma mark - Navigation初始化
 - (void)createNavigationWithRootViewController:(UIViewController*)root{
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:root];
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
 }
-- (void)createNavigationWithUIStoryboard:(NSString*)storyboard bundle:(nullable NSBundle*)bundle{
+- (void)createNavigationWithIntitalViewControllerAtStoryboard:(NSString*)storyboard bundle:(nullable NSBundle*)bundle{
     UIViewController *rootVC = [[UIStoryboard storyboardWithName:storyboard bundle:bundle] instantiateInitialViewController];
-    [self createNavigationWithRootViewController:rootVC];
-}
-- (void)createNavigationWithNibClass:(NSString*)nib bundle:(nullable NSBundle*)bundle{
-    UIViewController *rootVC = [[NSClassFromString(nib) alloc] initWithNibName:nib bundle:bundle];
     [self createNavigationWithRootViewController:rootVC];
 }
 
@@ -74,7 +64,7 @@
 }
 - (void)applicationDidEnterBackground:(UIApplication *)application{
     // 已经后台
-    [[NSNotificationCenter defaultCenter] postNotificationName:kSigApplicationWillResignActive object:nil];
+    [[NSNotificationCenter defaultCenter] postNotificationName:kSigApplicationWillEnterForeground object:nil];
 }
 - (void)applicationWillEnterForeground:(UIApplication *)application{
     // 即将前台
@@ -100,4 +90,19 @@
         }
     }
 }
+
+#pragma mark - UISceneSession lifecycle
+#if isUsingScene
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options  API_AVAILABLE(ios(13.0)){
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions  API_AVAILABLE(ios(13.0)){
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+#endif
 @end
