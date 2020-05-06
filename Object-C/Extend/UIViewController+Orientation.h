318919a48b8b897ae12230e11893c367cf8f464b
vinson1105 <vinson1105@foxmail.com>
2020-05-06 18:03:16 +0800

    增加注释和layout切换接口

--- Object-C/Extend/UIViewController+Orientation.h
+++ Object-C/Extend/UIViewController+Orientation.h
@@ -1,17 +1,28 @@
-//
-//  UIViewController+Orientation.h
-//  Bolts
-//
-//  Created by VINSON on 2019/10/14.
-//
+/**
+ 2020-05-06
+ 1、调整横竖屏状态获取
+ 2、新增旋转对应的layout接口
+ 注意：
+ 1、UIApplicationWillChangeStatusBarOrientationNotification/UIApplicationWillChangeStatusBarOrientationNotification相关在iOS13之后会被废弃，后续需要及时更新
+ 2、UIApplicationWillChangeStatusBarOrientationNotification存在触发两次的情况，后续需要及时修复
+ 
+ 2019-10-14 UIViewController的横屏竖控制
+ */
 
 #import <UIKit/UIKit.h>
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIViewController (Orientation)
-@property (nonatomic,assign) BOOL landscape;
-@property (nonatomic,copy,nullable) void(^landscapeChangedBlock)(BOOL onLandscape);
-- (void) installOrientation:(BOOL)install;
+@property (nonatomic,assign,readonly) BOOL isLandscape;
+@property (nonatomic,copy,nullable) void(^onOrientationWillChange)(void);
+@property (nonatomic,copy,nullable) void(^onOrientationDidChanged)(void);
+@property (nonatomic,strong,nullable) NSArray<NSLayoutConstraint*> *portraitLayouts;
+@property (nonatomic,strong,nullable) NSArray<NSLayoutConstraint*> *landscapeLayouts;
+
+- (void)installOrientation:(BOOL)install;
+- (void)landscapeLeft;
+- (void)landscapeRight;
+- (void)portrait;
 @end
 
 NS_ASSUME_NONNULL_END
--- Object-C/Extend/UIViewController+Orientation.m
+++ Object-C/Extend/UIViewController+Orientation.m
@@ -1,49 +1,83 @@
-//
-//  UIViewController+Orientation.m
-//  Bolts
-//
-//  Created by VINSON on 2019/10/14.
-//
-
 #import "UIViewController+Orientation.h"
 #import <objc/runtime.h>
 
+#define kLandscape                  @"landscape"
+#define kOnOrientationWillChange    @"onOrientationWillChange"
+#define kOnOrientationDidChanged    @"onOrientationDidChanged"
+#define kPortraitLayouts            @"portraitLayouts"
+#define kLandscapeLayouts           @"landscaoeLayouts"
+
+@interface UIViewController()
+@property (nonatomic,assign) BOOL landscape;
+@end
+
 @implementation UIViewController (Orientation)
+#pragma mark - Public
 - (void)installOrientation:(BOOL)install{
     if(install){
         [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onOrientationDidChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onOrientationWillChange:) name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onOrientationDidChanged:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
     }
     else{
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
     }
 }
+- (void)landscapeLeft{
+    [self toChangeOrientation:UIInterfaceOrientationLandscapeLeft];
+}
+- (void)landscapeRight{
+    [self toChangeOrientation:UIInterfaceOrientationLandscapeRight];
+}
+- (void)portrait{
+    [self toChangeOrientation:UIInterfaceOrientationPortrait];
+}
 
+#pragma mark - Property
 - (void)setLandscape:(BOOL)landscape{
-    if(landscape == [self landscape]){
-        return;
-    }
-    [self toLandscape:landscape];
-    objc_setAssociatedObject(self, @"XXlandscape", @(landscape), OBJC_ASSOCIATION_COPY_NONATOMIC);
+    objc_setAssociatedObject(self, kLandscape, @(landscape), OBJC_ASSOCIATION_ASSIGN);
 }
 - (BOOL)landscape{
-    return [objc_getAssociatedObject(self, @"XXlandscape") boolValue];
+    return [objc_getAssociatedObject(self, kLandscape) boolValue];
+}
+- (BOOL)isLandscape{
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    return orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight;
+}
+- (void)setOnOrientationDidChanged:(void (^)(void))onOrientationDidChanged{
+    objc_setAssociatedObject(self, kOnOrientationDidChanged, onOrientationDidChanged, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+- (void (^)(void))onOrientationDidChanged{
+    return objc_getAssociatedObject(self, kOnOrientationDidChanged);
+}
+- (void)setOnOrientationWillChange:(void (^)(void))onOrientationWillChange{
+    objc_setAssociatedObject(self, kOnOrientationWillChange, onOrientationWillChange, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+- (void (^)(void))onOrientationWillChange{
+    return objc_getAssociatedObject(self, kOnOrientationWillChange);
 }
 
-- (void)setLandscapeChangedBlock:(void (^)(BOOL))landscapeChangedBlock{
-    objc_setAssociatedObject(self, @"XXlandscapeChangedBlock", landscapeChangedBlock, OBJC_ASSOCIATION_COPY_NONATOMIC);
+- (void)setPortraitLayouts:(NSArray<NSLayoutConstraint *> *)portraitLayouts{
+    objc_setAssociatedObject(self, kPortraitLayouts, portraitLayouts, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
-- (void (^)(BOOL))landscapeChangedBlock{
-    return objc_getAssociatedObject(self, @"XXlandscapeChangedBlock");
+- (NSArray<NSLayoutConstraint *> *)portraitLayouts{
+    return objc_getAssociatedObject(self, kPortraitLayouts);
+}
+- (void)setLandscapeLayouts:(NSArray<NSLayoutConstraint *> *)landscapeLayouts{
+    objc_setAssociatedObject(self, kLandscapeLayouts, landscapeLayouts, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (NSArray<NSLayoutConstraint *> *)landscapeLayouts{
+    return objc_getAssociatedObject(self, kLandscapeLayouts);
 }
 
-- (void)toLandscape:(BOOL)landscape{
+- (void)toChangeOrientation:(int)orientation{
     if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
         SEL selector = NSSelectorFromString(@"setOrientation:");
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
         [invocation setSelector:selector];
         [invocation setTarget:[UIDevice currentDevice]];
-        int val = landscape ? UIInterfaceOrientationLandscapeRight : UIInterfaceOrientationPortrait;
+        int val = orientation;
         [invocation setArgument:&val atIndex:2];
         [invocation invoke];
     }
@@ -51,19 +85,33 @@
         NSLog(@"[UIViewController+Orientation] Error occurred! (LINE:%d)", __LINE__);
     }
 }
+- (void)onOrientationWillChange:(NSNotification*)notification {
+    /// FIXME: 会触发两次
+    if(nil != self.onOrientationWillChange){
+        self.onOrientationWillChange();
+    }
+}
 - (void)onOrientationDidChanged:(NSNotification*)notification {
-    CGRect frame = self.view.frame;
-    BOOL isLandscape = self.landscape;
-    if ([[UIDevice currentDevice] orientation] == UIInterfaceOrientationPortrait || [[UIDevice currentDevice] orientation] == UIInterfaceOrientationPortraitUpsideDown) {
-        if(frame.size.width > frame.size.height || !isLandscape) return;
+    /// FIXME: 会触发两次，现在增加判断避免重复调用
+    BOOL isLandscape    = self.isLandscape;
+    if(self.landscape == isLandscape){
+        return;
     }
-    else {
-        if(frame.size.width < frame.size.height || isLandscape)  return;
+    self.landscape = isLandscape;
+    
+    NSArray *nonactive  = isLandscape ? self.portraitLayouts : self.landscapeLayouts;
+    NSArray *active     = isLandscape ? self.landscapeLayouts : self.portraitLayouts;
+    if(active && nonactive){
+        [NSLayoutConstraint deactivateConstraints:nonactive];
+        [NSLayoutConstraint activateConstraints:active];
+
+        [UIView animateWithDuration:0.03 animations:^{
+            [self.view layoutIfNeeded];
+        }];
     }
-    isLandscape = !isLandscape;
-    objc_setAssociatedObject(self, @"XXlandscape", @(isLandscape), OBJC_ASSOCIATION_COPY_NONATOMIC);
-    if(nil != self.landscapeChangedBlock){
-        self.landscapeChangedBlock(isLandscape);
+    
+    if(nil != self.onOrientationDidChanged){
+        self.onOrientationDidChanged();
     }
 }
 @end
