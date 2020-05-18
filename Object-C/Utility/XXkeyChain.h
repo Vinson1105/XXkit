eaf3eb0c3d2af5abac240308f313f5762915769a
Vinson1105 <386820683@qq.com>
2020-05-19 06:56:27 +0800

    新增钥匙串文件

---
+++ Object-C/Utility/XXkeyChain.h
@@ -0,0 +1,16 @@
+//
+//  XXkeyChain.h
+//  XXkitProject
+//
+//  Created by 郭文轩 on 2020/5/19.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XXkeyChain : NSObject
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Object-C/Utility/XXkeyChain.m
@@ -0,0 +1,18 @@
+//
+//  XXkeyChain.m
+//  XXkitProject
+//
+//  Created by 郭文轩 on 2020/5/19.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import "XXkeyChain.h"
+#import <Security/SecItem.h>
+#import <Security/SecBase.h>
+
+@implementation XXkeyChain
++ (BOOL)update{
+    
+    return YES;
+}
+@end
