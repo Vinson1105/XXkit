c5319b2432897fad29e909f8c28138f1fe616f2f
vinson1105 <vinson1105@foxmail.com>
2020-05-22 17:58:52 +0800

    修改引用的头文件路径

--- Object-C/Shell/XXloginShell.h
+++ Object-C/Shell/XXloginShell.h
@@ -14,7 +14,7 @@
  */
 
 #import <UIKit/UIKit.h>
-#import "../UITextField/XXtextFieldShell.h"
+#import "./XXtextFieldShell.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
--- Object-C/Shell/XXverificationCodeShell.h
+++ Object-C/Shell/XXverificationCodeShell.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 #import "XXlimitButtonShell.h"
-#import "../UITextField/XXtextFieldShell.h"
+#import "./XXtextFieldShell.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
