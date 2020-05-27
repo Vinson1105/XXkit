8698b052a4ffb6621bbb6ff4988c62c745f90578
vinson1105 <vinson1105@foxmail.com>
2020-05-27 16:54:55 +0800

    Update UIButton+StateSettings.m

--- Object-C/Category/UIButton+StateSettings.m
+++ Object-C/Category/UIButton+StateSettings.m
@@ -1,9 +1,10 @@
-//
-//  UIButton+XX.m
-//  Bolts
-//
-//  Created by VINSON on 2019/10/16.
-//
+/**
+ 关于stateToSettings
+ state:{
+    BorderColor
+    BackgroundColor
+ }
+ */
 
 #import "UIButton+StateSettings.h"
 #import <objc/runtime.h>
@@ -21,8 +22,9 @@
 + (void)load{
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        //[XXocUtils replaceMethod:[self class] src:@selector(setState:) dest:@selector(xx_setState:)];
         [XXocUtils replaceMethod:[self class] src:@selector(setSelected:) dest:@selector(xx_setSelected:)];
+        [XXocUtils replaceMethod:[self class] src:@selector(setHighlighted:) dest:@selector(xx_setHighlighted:)];
+        [XXocUtils replaceMethod:[self class] src:@selector(setEnabled:) dest:@selector(xx_setEnabled:)];
     });
 }
 
@@ -39,71 +41,62 @@
 }
 
 - (void)setBorderColor:(UIColor *)color forState:(UIControlState)state{
-    NSMutableDictionary *allSettings = self.stateToSettings;
-    NSMutableDictionary *settings = allSettings[@(state)];
-    if(nil == settings){
-        settings = [NSMutableDictionary new];
-        allSettings[@(state)] = settings;
-    }
-    settings[kBorderColor] = color;
-    if(self.state == state){
-        self.layer.borderColor = color.CGColor;
-    }
+    [XXocUtils mainThreadProcess:^{
+        NSMutableDictionary *settings = self.stateToSettings[@(state)];
+        if(nil == settings){
+            settings = [NSMutableDictionary new];
+            self.stateToSettings[@(state)] = settings;
+        }
+        settings[kBorderColor] = color;
+        if(self.state == state){
+            self.layer.borderColor = color.CGColor;
+        }
+    }];
 }
 - (void)setBackgroundColor:(UIColor *)color forState:(UIControlState)state{
-    NSMutableDictionary *allSettings = self.stateToSettings;
-    NSMutableDictionary *settings = allSettings[@(state)];
-    if(nil == settings){
-        settings = [NSMutableDictionary new];
-        allSettings[@(state)] = settings;
-    }
-    settings[kBackgroundColor] = color;
-    if(self.state == state){
-        self.backgroundColor = color;
-    }
+    [XXocUtils mainThreadProcess:^{
+        NSMutableDictionary *settings = self.stateToSettings[@(state)];
+        if(nil == settings){
+            settings = [NSMutableDictionary new];
+            self.stateToSettings[@(state)] = settings;
+        }
+        settings[kBackgroundColor] = color;
+        if(self.state == state){
+            self.backgroundColor = color;
+        }
+    }];
 }
 
 - (void)xx_setSelected:(BOOL)selected{
     [self xx_setSelected:selected];
     [self checkState];
 }
-//- (void)xx_setEnabled:(BOOL)enabled{
-//    [self xx_setEnabled:enabled];
-//    [self checkState];
-//}
-//- (void)xx_setHighlighted:(BOOL)highlighted{
-//    [self xx_setHighlighted:highlighted];
-//    [self checkState];
-//}
-- (void)xx_setState:(UIControlState)state{
-    [self xx_setState:state];
+- (void)xx_setEnabled:(BOOL)enabled{
+    [self xx_setEnabled:enabled];
+    [self checkState];
+}
+- (void)xx_setHighlighted:(BOOL)highlighted{
+    [self xx_setHighlighted:highlighted];
     [self checkState];
 }
 
 - (void) checkState{
     NSLog(@"[StateSettings] text:%@ state:%lu", self.titleLabel.text, (unsigned long)self.state);
-//    NSMutableDictionary *allSettings = self.stateToSettings;
-//    NSEnumerator *enumer = allSettings.keyEnumerator;
-//    NSNumber *stateNumber = nil;
-//    while (nil != (stateNumber = enumer.nextObject)) {
-//        UIControlState state = [stateNumber integerValue];
-//        if (state == UIControlStateNormal) {
-//            if(UIControlStateHighlighted == self.state || state == self.state){
-//                NSDictionary *settings = allSettings[stateNumber];
-//                if(settings[kBorderColor]) self.layer.borderColor = [settings[kBorderColor] CGColor];
-//                if(settings[kBackgroundColor]) self.backgroundColor = settings[kBackgroundColor];
-//                break;
-//            }
-//        }
-//        else{
-//            if((self.state | state) == self.state){
-//                NSDictionary *settings = allSettings[stateNumber];
-//                if(settings[kBorderColor]) self.layer.borderColor = [settings[kBorderColor] CGColor];
-//                if(settings[kBackgroundColor]) self.backgroundColor = settings[kBackgroundColor];
-//                break;
-//            }
-//        }
-//    }
+    
+    NSMutableDictionary *settings = self.stateToSettings[@(self.state)];
+    if(nil == settings){
+        return;
+    }
+    
+    UIColor *backgroundColor = settings[kBackgroundColor];
+    if(nil != backgroundColor){
+        [self setBackgroundColor:backgroundColor];
+    }
+    
+    UIColor *borderColor = settings[kBorderColor];
+    if(nil != borderColor){
+        self.layer.borderColor = borderColor.CGColor;
+    }
 }
 
 @end
