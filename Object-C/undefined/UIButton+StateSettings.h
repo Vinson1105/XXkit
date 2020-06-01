8e754d17218b5709f5c7a850bd2ae81b87be4591
vinson1105 <vinson1105@foxmail.com>
2020-06-01 17:58:54 +0800

    删除多余文件

--- Object-C/undefined/UIButton+StateSettings.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-NS_ASSUME_NONNULL_BEGIN
-
-@interface UIButton(StateSettings)
--(void)setBorderColor:(UIColor*)color forState:(UIControlState)state;
--(void)setBackgroundColor:(UIColor*)color forState:(UIControlState)state;
-@end
-
-NS_ASSUME_NONNULL_END
