3e4b4c11e78e6a2fd7ef87124b9b6dc44da63264
vinson1105 <vinson1105@foxmail.com>
2020-06-05 17:54:00 +0800

    Update XXocUtils.h

--- Object-C/XXocUtils.h
+++ Object-C/XXocUtils.h
@@ -29,28 +29,6 @@
 #define XXOC_IS_KINDOF(obj,cls) (nil != obj && [obj isKindOfClass:[cls class]])
 #define XXOC_IS_STRING(obj)     XXOC_IS_KINDOF(obj,NSString)
 #define XXOC_IS_DICTIONARY(obj) XXOC_IS_KINDOF(obj,NSDictionary)
-
-//// 一些在K-V中有特殊作用的键
-///// XXshell中使用
-//#define XXSHELL_DEFAULE_CELL        @"_DefaultCell_"    // 默认cell类型
-//#define XXSHELL_DEFAULE_HEADER      @"_DefaultHeader_"  // 默认header类型
-//#define XXSHELL_DEFAULE_FOOTER      @"_DefaultFooter_"  // 默认footer类型
-//
-//#define XXSHELL_SECTION_HEADER      @"_Header_"         // section中header节点值
-//    #define XXSHELL_HEADER_TITLE    @"_Title_"          // header中的标题
-//
-//#define XXSHELL_SECTION_FOOTER      @"_Footer_"         // section中footer节点值
-//    #define XXSHELL_FOOTER_TITLE    @"_Title_"          // footer中的标题
-//
-//#define XXSHELL_SECTION_ROW         @"_Row_"            // section中row节点值
-//    #define XXSHELL_ROW_TEXT        @"_Text_"           // row中的text
-//
-///// XXstyle中使用
-//#define XXSTYLE_TEXT_COLOR          @"_TextColor_"
-//#define XXSTYLE_FONT_FAMILY         @"_FontFamily_"
-//#define XXSTYLE_FONT_SIZE           @"_FontSize_"
-//#define XXSTYLE_BACKGROUND_COLOR    @"_BackgroundColor_"
-//#define XXSTYLE_BACKGROUND_IMAGE    @"_BackgroundImage_"
 #endif
 
 #import <UIKit/UIKit.h>
