57c64cc7521ee706bbbac51d337e2672c47fbcc8
vinson1105 <vinson1105@foxmail.com>
2020-06-06 17:27:33 +0800

    基本功能整理完毕

--- Object-C/View/XXmaskView.h
+++ Object-C/View/XXmaskView.h
@@ -1,65 +1,26 @@
-/*
-    说明:
-    20181023
-    1. 在单例模式下,如果新的content与上一个相同,则沿用上一个的content和hideHandler
-    2. 默认背景是0.5Alpha,如果需要实现透明功能需要初始化一个,不建议直接修改单例
-    20181208
-    1. 从继承UIView更改为继承UIObject,将蒙层view私有化,避免继承uiview时加入到应用窗口中导致无法正确释放的问题
-*/
+/**
+ 2020.06.06
+ 1、增加单例接口，对代码重新整理，移除在hidden状态下，对content的强引用（想要使用content快捷显示maskview，可以使用UIView+MaskView）
+ 
+ 2018.10.23
+ 1、在单例模式下,如果新的content与上一个相同,则沿用上一个的content和hideHandler
+ 2、 默认背景是0.5Alpha,如果需要实现透明功能需要初始化一个,不建议直接修改单例
+ */
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface XXmaskView : UIView<UIGestureRecognizerDelegate>
-/// 方案一
-@property (nonatomic,assign) BOOL touchToHidden;
-@property (nonatomic,assign) BOOL animated;
-+ (XXmaskView*)maskViewWithContent:(UIView*)content;
-+ (XXmaskView*)maskViewWithContentAtCenter:(UIView*)content edgeInsets:(UIEdgeInsets)edgeInsets;
-+ (XXmaskView*)maskViewWithContentAtBottom:(UIView*)content edgeInsets:(UIEdgeInsets)edgeInsets;
-
-/// 方案二
+/** 单例 */
 + (XXmaskView*)sharedInstance;
+
+/**
+ 居中展示内容，通过hidden隐藏，隐藏之后会异常对view的相关引用
+ @param view 需要展示的内容
+ @param animated 动效（暂时没有实现）
+ */
 - (void)show:(UIView*)view animated:(BOOL)animated;
-
-/**
- * @brief    单例,使用单例时,在某些情况下可能出现无法显示的问题,暂时没有解决
- */
-//+ (instancetype) sharedInstance;
-
-/**
- * @brief    初始化
- * @param    content 在蒙层中显示的内容
- * @param    willHideBlock 蒙层将要隐藏时执行的回调,返回YES隐藏,否则不隐藏
- * @param    alpha 蒙层的透明度,默认为0.5
- */
-- (instancetype) initWithContent:(UIView*)content
-                    WillHideBlock:(nullable BOOL(^)(void))willHideBlock
-                            Alpha:(CGFloat)alpha;
-
-/**
- * @brief    显示并设置蒙层中内容,此函数配合单例使用
- * @param    content 在蒙层中显示的内容
- * @param    willHideBlock 蒙层将要隐藏时执行的回调,返回YES隐藏,否则不隐藏
- */
-- (void) show:(UIView* )content WillHideBlock:(BOOL(^ __nullable)(void))willHideBlock;
-
-/**
- * @brief   显示
- */
-- (void) show;
-
-/**
- * @brief   隐藏
- */
-- (void) hide;
-
-/**
- * @brief   判断蒙层是否隐藏
- * @return  返回YES,隐藏中
- */
-//- (BOOL) isHidden;
 @end
 
 NS_ASSUME_NONNULL_END
--- Object-C/View/XXmaskView.m
+++ Object-C/View/XXmaskView.m
@@ -7,159 +7,82 @@
 //
 
 #import "XXmaskView.h"
+#import "../XXocUtils.h"
 
 typedef BOOL (^WillHideBlock)(void);      // 隐藏事件处理block类型
 
+static XXmaskView *_instance = nil;
+
+
 @interface XXmaskView()
-/** 蒙层的父类 */
-@property (weak,nonatomic) UIView *superView;
-/** 蒙层自身view */
-//@property (strong,nonatomic) UIView *maskView;
-/** 蒙层的透明度 */
-@property (nonatomic) CGFloat maskAlpha;
-/** 蒙层中显示的内容 */
-@property (weak,nonatomic) UIView *content;
-/** 蒙层将要隐藏时执行的回调 */
-@property (copy) WillHideBlock willHideBlock;
+@property (nonatomic,weak) UIView *content;
+@property (nonatomic,strong) UITapGestureRecognizer *tapGestureRecognizer;
 @end
 
 @implementation XXmaskView
-
-#pragma mark - 构建和析构
-- (instancetype)init
-{
++ (XXmaskView*)sharedInstance{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _instance = [XXmaskView new];
+    });
+    return _instance;
+}
+- (instancetype)init{
     self = [super init];
     if (self) {
-        // [1] 初始化蒙层,满屏,50%的透明度
-        self.frame = [[UIScreen mainScreen] bounds];
-        
-        // [2] 把蒙层添加到应用的窗口中
-        id<UIApplicationDelegate> delegate = [UIApplication sharedApplication].delegate;
-        self.superView = delegate.window;
-        [self.superView addSubview:self];
-        [self setHidden:YES];
-        
-        // [3] 添加点击事件
-        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapAction:)];
-        [self addGestureRecognizer:tap];
-        tap.delegate = self;
-        
-        // [4] 监听横竖屏切换
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(application_onStatusBarOrientationChanged:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+        self.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
+        self.translatesAutoresizingMaskIntoConstraints = NO;
+        self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTap:)];
+        self.tapGestureRecognizer.numberOfTouchesRequired = 1;
+        self.tapGestureRecognizer.numberOfTapsRequired = 1;
+        self.tapGestureRecognizer.delegate = self;
+        [self addGestureRecognizer:self.tapGestureRecognizer];
+        //self.userInteractionEnabled = YES;
     }
     return self;
 }
-// 以指定的alpha值进行初始化
-- (instancetype) initWithContent:(UIView*)content WillHideBlock:(nullable BOOL(^)(void))willHideBlock Alpha:(CGFloat)alpha{
-    self = [self init];
-    if (self) {
-        self.content = content;
-        self.willHideBlock = willHideBlock;
-        self.maskAlpha = alpha;
-        self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:alpha];
-        [self addSubview:content];
+- (void)show:(UIView*)view animated:(BOOL)animated{
+    if(_content){
+        [_content removeFromSuperview];
+        _content = nil;
     }
-    return self;
-}
-// 析构
-- (void)dealloc
-{
-    if (nil != self.superView) {
-        /**
-         由于蒙层是添加到应用的窗口中,在整个应用生命周期中,该窗口不会被释放
-         需要在本类释放时,将蒙层从应用窗口中移除,避免不是使用单例的情况下内存泄漏
-         */
-        [self removeFromSuperview];
-    }
-}
-
-#pragma mark - 布局
-
-
-#pragma mark - 公开方法: <单例访问> <显示蒙层> <隐藏蒙层> <是否隐藏判断>
-// 单例
-+ (instancetype) sharedInstance{
-    static XXmaskView *instance = nil;
-    static dispatch_once_t onceToken;
+    _content = view;
+    _content.translatesAutoresizingMaskIntoConstraints = NO;
     
-    dispatch_once(&onceToken, ^{
-        instance = [[self alloc] init];
-    });
+    [self addSubview:view];
+    [XXocUtils view:view centerAt:self];
+     
+    UIWindow *window = [UIApplication sharedApplication].delegate.window;
+    [window addSubview:self];
+    [XXocUtils view:self margin:0 fillAt:window];
     
-    return instance;
-}
-// 显示蒙层
-- (void) show:(UIView*)content WillHideBlock:(BOOL(^ __nullable)(void))willHideBlock{
-    if(content != self.content){
-        // 移除上一个content
-        if(nil != self.content){
-            [self.content removeFromSuperview];
-        }
-        
-        // 加入新的content
-        [self addSubview:content];
-        self.content = content;
-        self.willHideBlock = willHideBlock;
-    }
-    [self show];
-}
-// 显示蒙层
-- (void) show{
-    if (self.maskAlpha != 0) {
-        __weak typeof(self) weakSelf = self;
-        weakSelf.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
-        [UIView animateWithDuration:0.2 animations:^{
-            weakSelf.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:weakSelf.maskAlpha];
-        }];
-    }
-    
-    [self setHidden:NO];
-}
-// 隐藏蒙层
-- (void) hide{
-    if(nil != self.willHideBlock && !self.willHideBlock())
-        return;
-    
-    if (self.maskAlpha != 0) {
-        __weak typeof(self) weakSelf = self;
-        [UIView animateWithDuration:0.2 animations:^{
-            weakSelf.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
-        } completion:^(BOOL finished){
-            [weakSelf setHidden:YES];
+    if(animated){
+        [UIView animateWithDuration:3 animations:^{
+            self.hidden = NO;
         }];
     }
     else{
-        [self setHidden:YES];
+        self.hidden = NO;
     }
 }
-// 是否隐藏判断
-//- (BOOL) isHidden{
-//    return nil == self.maskView ? YES : self.maskView.isHidden;
-//}
-
-#pragma mark - 手势事件
-- (void)tapAction:(UITapGestureRecognizer *)selfTap{
-    CGPoint point = [selfTap locationInView:self];
-    if (nil != self.content) {
-        if(!CGRectContainsPoint(self.content.frame, point))
-            [self hide];
-    }
-    else{
-        [self hide];
+- (void)setHidden:(BOOL)hidden{
+    [super setHidden:hidden];
+    if(hidden && _content){
+        [_content removeFromSuperview];
+        _content = nil;
     }
 }
 
-#pragma mark - 代理: <UIGestureRecognizerDelegate>
--(BOOL)gestureRecognizer:(UIGestureRecognizer*)gestureRecognizer shouldReceiveTouch:(UITouch*)touch{
-    CGPoint point = [touch locationInView:self.superView];
-    if (nil != self.content && CGRectContainsPoint(self.content.frame, point)) {
+#pragma mark - <手势>
+-(void)onTap:(UITapGestureRecognizer*)recognizer{
+    self.hidden = YES;
+}
+
+#pragma mark - <UIGestureRecognizerDelegate>
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
+    if (_content && [touch.view isDescendantOfView:_content]) {
         return NO;
     }
     return YES;
-}
-
-#pragma mark - 通知处理: <横竖屏通知>
-- (void)application_onStatusBarOrientationChanged:(NSNotification *)notification{
-    self.frame = [[UIScreen mainScreen] bounds];
 }
 @end
