ddc3746dff29e8d2f099d295d266dfa4d44dec2f
vinson1105 <vinson1105@foxmail.com>
2020-06-12 17:59:25 +0800

    Update XXh26xEncoder.h

--- Object-C/AV/XXh26xEncoder.h
+++ Object-C/AV/XXh26xEncoder.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 #import "XXavFormat.h"
+#import <AVFoundation/AVFoundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 @interface XXh26xEncoder : NSObject
@@ -8,6 +9,6 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic,copy,nullable) void(^onCompressionError)(XXh26xEncoder *encoder, NSError *error);
 
 + (nullable XXh26xEncoder*)encoderWithFormat:(XXvideoFormat*)format;
-- (BOOL)encode:(CVImageBuffer)buffer timestamp:(NSTimeInterval)timestamp;
+- (BOOL)encode:(CVImageBufferRef)buffer timestamp:(NSTimeInterval)timestamp;
 @end
 NS_ASSUME_NONNULL_END
