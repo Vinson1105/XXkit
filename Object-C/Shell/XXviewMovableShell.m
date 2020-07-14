7c03f2b9f7bb2f55129bcb73977470db9574e009
vinson1105 <vinson1105@foxmail.com>
2020-07-14 17:56:42 +0800

    Update XXviewMovableShell.m

--- Object-C/Shell/XXviewMovableShell.m
+++ Object-C/Shell/XXviewMovableShell.m
@@ -29,6 +29,20 @@
     [_view addGestureRecognizer:_panGesture];
 }
 - (void)onPanGesture:(UIPanGestureRecognizer*)recognizer{
-    
+    recognizer.cancelsTouchesInView = YES;
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+        NSLog(@"FlyElephant---视图拖动开始");
+    }
+    else if (recognizer.state == UIGestureRecognizerStateChanged) {
+        CGPoint translation = [recognizer translationInView:self.view.superview];
+        recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,recognizer.view.center.y + translation.y);
+        [recognizer setTranslation:CGPointZero inView:self.view.superview];
+    }
+    else if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled) {
+        NSLog(@"FlyElephant---视图拖动结束");
+    }
+    else{
+        
+    }
 }
 @end
