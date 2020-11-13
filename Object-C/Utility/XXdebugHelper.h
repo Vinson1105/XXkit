de248ae36762831884884d152a72f64849872597
vinson1105 <vinson1105@foxmail.com>
2020-11-13 17:59:43 +0800

    update

--- Object-C/Utility/XXdebugHelper.h
+++ Object-C/Utility/XXdebugHelper.h
@@ -12,7 +12,31 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface XXdebugHelper : NSObject
+/**
+ 将调试小组件初始化在哪个window上，可以动态切换window；
+ */
 +(void)install:(UIWindow*)window;
+
+/**
+ 设置可配置的属性；
+ type的可选值，字符‘=’为分割字符，暂不支持非分割时使用
+    - 字符串：string=默认值（aaa）
+    - 整型：int=默认值（100）
+    - 浮点：float=默认值（0.01）
+    - 布尔：bool=默认值（0、1）
+    - 单选：[可选项1,可选项2,可选项3]=默认值（可选项2）
+    - 多选：[可选项1,可选项2,可选项3]*=默认值（可选项1,可选项2）
+ 例如
+ string=AAA
+ int=16
+ float=0.01
+ bool=0
+ [A,B,C,D]=B
+ [A,B,C,D]*=A,B
+ 其中默认是都是可以缺省的
+ */
++(void)configPropertyName:(NSString*)name type:(NSString*)type;
++(nullable id)getPropertyWithName:(NSString*)name;
 @end
 
 NS_ASSUME_NONNULL_END
--- Object-C/Utility/XXdebugHelper.m
+++ Object-C/Utility/XXdebugHelper.m
@@ -12,12 +12,160 @@
 #import "../Category/UIView+Touchable.m"
 #import "../Category/UIView+Movable.h"
 
+
 static XXdebugHelper *_instance = nil;
+static NSString * const kSplitOfProperty = @"=";
+static NSString * const kSplitOfEnum = @",";
+static NSString * const kType = @"type";
+static NSString * const kDefault = @"default";
+static NSString * const kValue = @"value";
+
+// MARK: 通用属性
+typedef enum : NSUInteger {
+    XXpropertyTypeInt,
+    XXpropertyTypeFloat,
+    XXpropertyTypeBool,
+    XXpropertyTypeString,
+    XXpropertyTypeSingleEnum,
+    XXpropertyTypeMultiEnum,
+} XXpropertyType;
+@interface XXproperty : NSObject<NSCopying>
+@property (nonatomic,copy,readonly) NSString *infoString;
+@property (nonatomic,assign,readonly) XXpropertyType type;
+@property (nonatomic,copy,readonly,nullable) id def;
+@property (nonatomic,copy,nullable) id val;
++(nullable XXproperty*)propertyFromString:(NSString*)string;
+@end
+@implementation XXproperty
++(XXproperty*)propertyFromString:(NSString*)string{
+    XXproperty *property = [XXproperty new];
+    if(![property reset:string]){
+        return nil;
+    }
+    return property;
+}
+-(instancetype)initWithProperty:(XXproperty*)property{
+    self = [super init];
+    if(self){
+        _type = property.type;
+        _def = property.def?[property.def copy]:nil;
+        _val = property.val?[property.val copy]:nil;
+    }
+    return self;
+}
+-(BOOL)reset:(NSString*)string{
+    NSArray *nodes = [string componentsSeparatedByString:kSplitOfProperty];
+    if(nodes.count>3 || 0==nodes.count){
+        return NO;
+    }
+    
+    // 类型转换
+    XXpropertyType type = -1;
+    NSString *typeString = nodes[0];
+    if([typeString isEqualToString:@"int"]){
+        type = XXpropertyTypeInt;
+    }
+    else if([typeString isEqualToString:@"float"]){
+        type = XXpropertyTypeFloat;
+    }
+    else if([typeString isEqualToString:@"bool"]){
+        type = XXpropertyTypeBool;
+    }
+    else if([typeString isEqualToString:@"string"]){
+        type = XXpropertyTypeString;
+    }
+    else if([typeString hasPrefix:@"["]){
+        if([typeString hasSuffix:@"]"]){
+            type = XXpropertyTypeSingleEnum;
+        }
+        else if([typeString hasSuffix:@"]*"]){
+            type = XXpropertyTypeMultiEnum;
+        }
+        else{
+            
+        }
+    }
+    else{
+        
+    }
+    if(-1==type){
+        return NO;
+    }
+    _type = type;
+    
+    // 默认值
+    _def = nil;
+    if(nodes.count>=2){
+        _def = [[self toValueFromString:nodes[1] forPropertyType:type] copy];
+    }
+    
+    // 实际值
+    _val = [self.def copy];
+    
+    return YES;
+}
+-(nullable id)toValueFromString:(NSString*)string forPropertyType:(XXpropertyType)propertyType{
+    switch (propertyType) {
+        case XXpropertyTypeInt:
+            return @([string intValue]);
+        case XXpropertyTypeFloat:
+            return @([string floatValue]);
+        case XXpropertyTypeBool:
+            return @([string boolValue]);
+        case XXpropertyTypeString:
+            return string;
+        case XXpropertyTypeSingleEnum:
+            return string;
+        case XXpropertyTypeMultiEnum:
+            return [string componentsSeparatedByString:kSplitOfEnum];
+        default:
+            return nil;
+    }
+}
+- (nonnull id)copyWithZone:(nullable NSZone *)zone {
+    XXproperty *property = [[self.class allocWithZone:zone] initWithProperty:self];
+    return property;
+}
+@end
+
+// MARK: 枚举属性
+@interface XXproperty(Enum)
+@property (nonatomic,strong,readonly) NSArray *enum_availableItem;
+@end
+@implementation XXproperty(Enum)
++ (void)load{
+    [XXocUtils replaceMethod:[self class] src:@selector(enum_reset:) dest:@selector(reset:)];
+}
+- (BOOL)enum_reset:(NSString*)string{
+    
+}
+- (NSArray *)enum_availableItem{
+    
+}
+@end
 
 // MARK: 调试器数据模型
 @interface DebugSettingModel : NSObject
+@property (nonatomic,strong) NSMutableDictionary *nameToProperty;
 @end
 @implementation DebugSettingModel
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        self.nameToProperty = [NSMutableDictionary new];
+    }
+    return self;
+}
+-(void)configPropertyName:(NSString*)name type:(NSString*)type{
+    XXproperty *property = [XXproperty propertyFromString:type];
+    if(nil == property){
+        return;
+    }
+    self.nameToProperty[name] = property;
+}
+-(nullable id)getPropertyValueWithName:(NSString*)name{
+    if()
+}
 @end
 
 // MARK: 调试器设置页面
@@ -29,6 +177,7 @@ static XXdebugHelper *_instance = nil;
 
 @property (nonatomic,assign,readonly) BOOL popup;
 @property (nonatomic,assign) CGRect popupFromRect;
+@property (nonatomic,strong) XXtableViewShell *tableViewShell;
 @end
 @implementation DebugSettingView
 - (instancetype)init{
@@ -37,28 +186,53 @@ static XXdebugHelper *_instance = nil;
         _popup = NO;
         self.backgroundColor = UIColor.whiteColor;
         
-        // table
+        // cancel
+        self.retButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        self.retButton.translatesAutoresizingMaskIntoConstraints = NO;
+        [self.retButton setTitle:@"Cancel" forState:UIControlStateNormal];
+        [self.retButton setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+        self.retButton.layer.cornerRadius = 22.5;
+        self.retButton.backgroundColor = UIColor.redColor;
+        [self addSubview:self.retButton];
+        [self.retButton.leadingAnchor constraintEqualToAnchor:self.leadingAnchor constant:20].active = YES;
+        [self.retButton.trailingAnchor constraintEqualToAnchor:self.trailingAnchor constant:-20].active = YES;
+        [self.retButton.bottomAnchor constraintEqualToAnchor:self.bottomAnchor constant:-20].active = YES;
+        NSLayoutConstraint *constraint = [self.retButton.heightAnchor constraintEqualToConstant:45];
+        constraint.priority = 250;
+        constraint.active = YES;
+        [self.retButton addTarget:self action:@selector(onTouchUpInside:) forControlEvents:UIControlEventTouchUpInside];
         
         // ok
         self.okButton = [UIButton buttonWithType:UIButtonTypeCustom];
         self.okButton.translatesAutoresizingMaskIntoConstraints = NO;
         [self.okButton setTitle:@"OK" forState:UIControlStateNormal];
-        [self.okButton setTitleColor:UIColor.blueColor forState:UIControlStateNormal];
+        [self.okButton setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+        self.okButton.layer.cornerRadius = 22.5;
+        self.okButton.backgroundColor = UIColor.greenColor;
         [self addSubview:self.okButton];
-        [self.okButton.leadingAnchor constraintEqualToAnchor:self.leadingAnchor].active = YES;
-        [self.okButton.bottomAnchor constraintEqualToAnchor:self.bottomAnchor].active = YES;
-        [self.okButton.widthAnchor constraintEqualToAnchor:self.widthAnchor multiplier:0.5].active = YES;
-        [self.okButton.heightAnchor constraintEqualToConstant:45].active = YES;
+        [self.okButton.leadingAnchor constraintEqualToAnchor:self.leadingAnchor constant:20].active = YES;
+        [self.okButton.trailingAnchor constraintEqualToAnchor:self.trailingAnchor constant:-20].active = YES;
+        [self.okButton.bottomAnchor constraintEqualToAnchor:self.retButton.topAnchor constant:-10].active = YES;
+        constraint = [self.okButton.heightAnchor constraintEqualToConstant:45];
+        constraint.priority = 250;
+        constraint.active = YES;
+        [self.okButton addTarget:self action:@selector(onTouchUpInside:) forControlEvents:UIControlEventTouchUpInside];
         
-        // cancel
-        self.retButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        self.retButton.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.retButton setTitle:@"Cancel" forState:UIControlStateNormal];
-        [self.retButton setTitleColor:UIColor.grayColor forState:UIControlStateNormal];
-        [self.okButton.trailingAnchor constraintEqualToAnchor:self.trailingAnchor].active = YES;
-        [self.okButton.bottomAnchor constraintEqualToAnchor:self.bottomAnchor].active = YES;
-        [self.okButton.widthAnchor constraintEqualToAnchor:self.widthAnchor multiplier:0.5].active = YES;
-        [self.okButton.heightAnchor constraintEqualToConstant:45].active = YES;
+        // table
+        self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 0, 0) style:UITableViewStyleGrouped];
+        self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addSubview:self.tableView];
+        [self.tableView.leadingAnchor constraintEqualToAnchor:self.leadingAnchor].active = YES;
+        [self.tableView.trailingAnchor constraintEqualToAnchor:self.trailingAnchor].active = YES;
+        [self.tableView.topAnchor constraintEqualToAnchor:self.topAnchor].active = YES;
+        [self.tableView.bottomAnchor constraintEqualToAnchor:self.okButton.topAnchor constant:-20].active = YES;
+        
+        // table shell
+        self.tableViewShell = [XXtableViewShell new];
+        [self.tableViewShell shell:self.tableView];
+        [self.tableViewShell configSectionRowSystemStyle:UITableViewCellStyleDefault height:40];
+        [self.tableViewShell configSectionHeaderClass:nil loadType:-1 height:40];
+        [self.tableViewShell configSectionFooterClass:nil loadType:-1 height:0.01];
     }
     return self;
 }
@@ -104,6 +278,17 @@ static XXdebugHelper *_instance = nil;
         }
     }
 }
+-(void)onTouchUpInside:(UIButton*)button{
+    if(button == self.okButton){
+        
+    }
+    else if(button == self.retButton){
+        [self popdown];
+    }
+    else {
+        
+    }
+}
 @end
 
 // MARK: 调试器主控
@@ -111,7 +296,7 @@ static XXdebugHelper *_instance = nil;
 @property (nonatomic,weak) UIWindow *window;
 @property (nonatomic,strong) UIView *indicaterView;
 @property (nonatomic,strong) DebugSettingView *settingView;
-//@property (nonatomic,strong) XXviewMovableShell *viewMovableShell;
+@property (nonatomic,strong) NSMutableDictionary *nameToType;
 @end
 
 @implementation XXdebugHelper
@@ -124,6 +309,12 @@ static XXdebugHelper *_instance = nil;
 }
 +(void)install:(UIWindow*)window{
     [XXdebugHelper sharedInstance].window = window;
+}
++(void)configPropertyName:(NSString*)name type:(NSString*)type{
+    [[XXdebugHelper sharedInstance] configPropertyName:name type:type];
+}
++(nullable id)getPropertyWithName:(NSString*)name{
+    return [[XXdebugHelper sharedInstance] getPropertyWithName:name];
 }
 
 - (instancetype)init{
@@ -144,6 +335,11 @@ static XXdebugHelper *_instance = nil;
         
         // SettingView
         self.settingView = [DebugSettingView new];
+        
+        // name to type
+        self.nameToType = [NSMutableDictionary new];
+        [self configPropertyName:@"LogServer" type:@"string=192.168.0.1:32323"];
+        [self configPropertyName:@"LogServerEnabled" type:@"bool=0"];
     }
     return self;
 }
@@ -157,6 +353,12 @@ static XXdebugHelper *_instance = nil;
         [self.indicaterView removeFromSuperview];
     }
     self.settingView.window = window;
+}
+-(void)configPropertyName:(NSString*)name type:(NSString*)type{
+    self.nameToType[name] = type;
+}
+-(nullable id)getPropertyWithName:(NSString*)name{
+    return nil;
 }
 - (void)onIndicaterViewTouched{
     [self.settingView popupFromRect:self.indicaterView.frame];
