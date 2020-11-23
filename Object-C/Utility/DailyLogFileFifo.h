34a5fb41592a5d2b5e189ad6d230265ed8c00e48
Vinson1105 <386820683@qq.com>
2020-11-24 07:50:24 +0800

    create DailyLogFileFifo

---
+++ Object-C/Utility/DailyLogFileFifo.h
@@ -0,0 +1,17 @@
+//
+//  DailyLogFileFifo.h
+//  XXkitProject
+//
+//  Created by 郭文轩 on 2020/11/24.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import "XXlogger.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DailyLogFileFifo : XXfifoBase
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Object-C/Utility/DailyLogFileFifo.m
@@ -0,0 +1,13 @@
+//
+//  DailyLogFileFifo.m
+//  XXkitProject
+//
+//  Created by 郭文轩 on 2020/11/24.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import "DailyLogFileFifo.h"
+
+@implementation DailyLogFileFifo
+
+@end
