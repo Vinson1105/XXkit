c3bd9f1d222318b2c61e27d44e8e011e3c3f2315
vinson1105 <vinson1105@foxmail.com>
2020-11-24 18:00:42 +0800

    create NetworkFifo

---
+++ Object-C/Utility/NetworkFifo.m
@@ -0,0 +1,30 @@
+//
+//  NetworkFifo.m
+//  XXkitProject
+//
+//  Created by VINSON on 2020/11/24.
+//  Copyright © 2020 郭文轩. All rights reserved.
+//
+
+#import "NetworkFifo.h"
+#import "../../C/XXtcpSocket.h"
+
+static NSString * const kIP = @"ip";
+static NSString * const kPort = @"port";
+static NSString * const kWay = @"way";  // 暂时支持tcp
+
+@interface NetworkFifo()
+// tcp
+
+// udp
+@end
+
+@implementation NetworkFifo
+- (instancetype)initWithParam:(NSDictionary *)param{
+    self = [super init];
+    if(self){
+        
+    }
+    return self;
+}
+@end
