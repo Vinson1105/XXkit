71ccff189ec74d51ddb1026d1024697c41d38099
Vinson1105 <386820683@qq.com>
2021-03-20 08:00:22 +0800

    已完成

--- Qt/Utility/FileLogFifo.cpp
+++ Qt/Utility/FileLogFifo.cpp
@@ -1,13 +1,75 @@
 #include "FileLogFifo.h"
+#include "XXlogger.h"
+#include <QDateTime>
+#include <QFileInfo>
+#include <QDir>
 
-FileLogFifo::FileLogFifo(const QVariantMap &param)
-    : XXfifoBase (){
+#include <QFile>
 
+const char * const FileLogFifo::kLogDir = "LogDir";
+
+FileLogFifo::FileLogFifo(const QVariantMap &param, QObject *parent)
+    : XXfifoBase (parent)
+    , _file(nullptr){
+    if(param.contains(kLogDir)){
+        _file = openFile(param[kLogDir].toString(),QDateTime::currentDateTime().toString("yyyy-MM-hh"));
+    }
+    else{
+        XXlogger::instance()->message("param not contains \"%s\".", kLogDir);
+    }
+    this->moveToThread(&_writeThread);
+    _writeThread.start();
+    connect(this, SIGNAL(sigPush(QSharedPointer<QByteArray>)), this, SLOT(onPush(QSharedPointer<QByteArray>)), Qt::QueuedConnection);
+}
+FileLogFifo::~FileLogFifo(){
+    _writeThread.quit();
+    if(_file){
+        _file->close();
+        _file->deleteLater();
+        _file = nullptr;
+    }
 }
 
-XXfifoBase* FileLogFifo::create(const QVariantMap &param) const{
-    return new FileLogFifo(param);
-}
 void FileLogFifo::push(const QByteArray &data){
-
+    emit sigPush(QSharedPointer<QByteArray>(new QByteArray(data)));
 }
+
+QFile* FileLogFifo::openFile(const QString &dirPath, const QString &fileName){
+    QFileInfo dirInfo(dirPath);
+    if(!dirInfo.exists() || dirInfo.isFile()){
+        XXlogger::instance()->message("log dir path is a file. path:%s",dirPath.toLocal8Bit().data());
+        return nullptr;
+    }
+
+    if(!QDir().mkpath(dirPath)){
+        XXlogger::instance()->message("failure to create log dir. path:%s", dirPath.toLocal8Bit().data());
+        return nullptr;
+    }
+
+    QString filePath = "";
+    if(dirPath.endsWith('/')){
+        filePath = dirPath + fileName;
+    }
+    else{
+        filePath = dirPath + '/' + fileName;
+    }
+    QFile *file = new QFile(filePath);
+    if (!file->open(QFile::ReadWrite|QFile::Text|QFile::Append)) {
+        XXlogger::instance()->message("failure to open file. path:%s", filePath.toLocal8Bit().data());
+        delete file;
+        return nullptr;
+    }
+    //file->close();  // 为什么这里需要close一下？因为写入文件是在另一个线程中，那么需要在另一个线程中打开
+    return file;
+}
+void FileLogFifo::onPush(QSharedPointer<QByteArray> bytes){
+    if(nullptr == _file){
+        return;
+    }
+
+    if(_file->write(*bytes) < 0){
+        // 注意：这里写入时需要排除自身，否则存在死循环的情况！
+        XXlogger::instance()->message(this, "failure to write.");
+    }
+}
+
--- Qt/Utility/FileLogFifo.h
+++ Qt/Utility/FileLogFifo.h
@@ -3,17 +3,39 @@
 
 #include "XXlogger.h"
 #include <QFile>
+#include <QSharedPointer>
+#include <QThread>
 
 class FileLogFifo : public XXfifoBase{
+    Q_OBJECT
 public:
-    FileLogFifo(const QVariantMap &param);
+    FileLogFifo(const QVariantMap &param, QObject *parent = nullptr);
     ~FileLogFifo();
 
-    XXfifoBase* create(const QVariantMap &param) const;
     void push(const QByteArray &data);
+
+public: // 一些公开常量
+    static const char * const kLogDir;
+
+private:
+    /**
+     * @brief openFile 根据目录和文件名称组成路径打开文件
+     * @param dirPath 文件对应的目录
+     * @param fileName 文件名称
+     * @return 打开失败将返回nullptr
+     */
+    QFile* openFile(const QString &dirPath, const QString &fileName);
+
+    /**
+     * 由于push函数可能存在多线程调用的情况，那么在多线程下直接写文件可能会导致一些异常情况；
+     * 这里就采取了信号-槽处理，槽直接放到固定线程中，使用事件循环做一个写触发队列；
+     */
+    Q_SIGNAL void sigPush(QSharedPointer<QByteArray> bytes);
+    Q_SLOT void onPush(QSharedPointer<QByteArray> bytes);
 
 private:
     QFile *_file;
+    QThread _writeThread;
 };
 
 #endif // FILELOGFIFO_H
